057a25cc9c3069b4ff8f7e1169af54ffafb9cfe7
goutarouh <gouta.gorogoro@gmail.com>
2023-06-10 19:59:32 +0900

    calendar icon表示

--- app/src/main/java/com/github/goutarouh/notioncalendar/ui/AppHeader.kt
+++ app/src/main/java/com/github/goutarouh/notioncalendar/ui/AppHeader.kt
@@ -1,6 +1,10 @@
 package com.github.goutarouh.notioncalendar.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -13,6 +17,16 @@ fun AppHeader(
     TopAppBar(
         title = {
             Text(text = title)
+        },
+        actions = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = ""
+                )
+            }
         }
     )
 }
